e67ba9df00582b8678cd933ef5eee8fa9fdeeaf6
Shreya S Rao <shreya.rao@zapcg.com>
2025-06-18 12:37:12 +0530

    updated

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator fucnctions for the app
+"""
+
+def add(x, y):
+    """
+    Adds two numbers together.
+    """
+    return x + y
+
+def subtract(x, y):
+    """
+    Subtracts second number from first.
+    """
+    return y - x
