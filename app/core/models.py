68e2e2169e83e760b4c22b2489a93b17ddaa93ce
Shreya S Rao <shreya.rao@zapcg.com>
2025-06-19 16:15:14 +0530

    core/models file

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,8 @@
 """Database models."""
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 
 class UserManager(BaseUserManager):
